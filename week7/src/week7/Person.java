dcfca21d0a76c52f73dc1653b4dc9cc8577805d2
Ahmed A <aalwailia@gmail.com>
2017-11-27 15:14:31 +0000

    update
    update

--- week7/src/week7/Person.java
+++ week7/src/week7/Person.java
@@ -1,50 +1,36 @@
 package week7;
 
 import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.Year;
 import java.util.Calendar;
+
 
 
 public class Person {
 	static String name;
-	static Date DoB;
+	static LocalDate DoB;//use localDate as calculations are easier to be made and Date has been removed
 	
-	Person(String name, Date DoB){
+	Person(String name, LocalDate DoB){//constructor
 		
-		this.name = name;
+		this.name = name;//intialise the variable name with the constructor
 		
-		this.DoB = DoB;
+		this.DoB = DoB;//initialise the variable date of birth with the constructor
 	
 	}
 	
-	int getAge(){
-	    Calendar today = Calendar.getInstance();
-	    int todayYear = today.get(Calendar.YEAR);
-	    int todayMon = today.get(Calendar.MONTH);
-	    int todayDay = today.get(Calendar.DATE);
-	    @SuppressWarnings("deprecation")
-		int birthDateDayOfYear = DoB.getYear();
-	    int birthMon = DoB.getMonth();
-	    int birthDay = DoB.getDate();
-	    int age = todayYear - birthDateDayOfYear;
-	    
-	    if(todayDay < birthDay) {
-    			return age = age - 1;
-	    }
-	    else if(todayMon < birthMon) {
-	    		return age = age - 1;
-	    }
-	    else {
-	    		return age;
-	    }
+	String getName() {
+		return name;
 	}
 	
-	public String toString() {
-		return "Name: " + name + " Age: " + getAge() + " Date of Birth: " + DoB;
+	int getAge(){//return the age of the Person
+	    LocalDate now = LocalDate.now();//get todays LocalDate 'now'
+	    return Period.between(DoB, now).getYears();//calucaltion to find the years between today and the date of birth
 	}
 	
-//	public static void main(String[] args) {
-//		Person ahmed = new Person("ahmed", new Date(1996, 12, ));
-//		System.out.println(ahmed.getAge());
-//	}
+	public String toString() {//toString() method to return all the functions into a string 
+		return "Name: " + name + " | Age: " + getAge() + " | Date of Birth: " + DoB;
+	}
 	
 }
